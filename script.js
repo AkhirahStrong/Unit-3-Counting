8032979b1abc4487f58c80984f63e55126b46633
Akhirahstrong <codeclubdetroit@gmail.com>
2023-04-26 18:48:55 -0400

    Add comments

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*Use a for loop to log numbers from 1 to 10, then another 
+for loop to count down, logging numbers from 10 to 1.*/
 for (let i = 1; i <= 10; i++) {
   console.log(i);
 }
@@ -6,6 +8,7 @@ for (let i = 10; i >= 1; i--) {
   console.log(i);
 }
 
+/**/
 let i = 1;
 
 while (i <= 10) {
@@ -20,6 +23,8 @@ while (j >= 1) {
   j--;
 }
 
+/*Repeat with a do while loop.
+ */
 let k = 1;
 
 do {
@@ -34,6 +39,9 @@ do {
   l--;
 } while (l >= 1);
 
+/*Create this array: const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]. 
+Use a for…of loop to loop through the array and log all the numbers. 
+(Note: we’re not asking you to also count down on this one.)*/
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 for (const number of numbers) {
